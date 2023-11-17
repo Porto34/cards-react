93a694d08beff36a2579534b8a1492267566706e
Sena <Sena@MEDAPRCSGFSD459.FormacionSena.RED>
2023-11-17 08:49:08 -0500

    Asi no

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Header from './Header.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header> h1props.lenguaje</Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import 'header.css'
+
+function Header(){
+    return(
+        <>
+        <h1 style={{color: props.lenguaje}}>Eri gay{props.lenguaje}</h1>
+        </>
+    )
+}
+export default Header
